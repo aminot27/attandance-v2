be5f54842ca1d353b4e414bcb7948cbec75e558c
aminot27 <ajhonjtc26@gmail.com>
2024-02-12 15:27:18 -0500

    adding

--- apps/warehouse/api_views/scan_dni_views.py
+++ apps/warehouse/api_views/scan_dni_views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from drf_yasg.utils import swagger_auto_schema
+from twilio.base.exceptions import TwilioRestException
+
 from apps.warehouse.models.student_model import Student
 from apps.warehouse.models.shift_model import Shift
 from apps.warehouse.models.attendance_record_model import AttendanceRecord
@@ -38,11 +40,22 @@ class ScanDniView(APIView):
                     'message': 'No hay turnos activos en este momento o el estudiante no está asignado a ningún turno.'
                 }, status=status.HTTP_200_OK)
 
-            attendance_record, created = AttendanceRecord.objects.get_or_create(
-                student=student,
-                shift=current_shift,
-                defaults={'status': 'Falta'}  # Inicializa con 'Absent'
-            )
+            today_date = localtime(now()).date()
+            try:
+                attendance_record = AttendanceRecord.objects.get(
+                    student=student,
+                    shift=current_shift,
+                    entry_time__date=today_date  # Filtra por la fecha de entry_time
+                )
+                created = False
+            except AttendanceRecord.DoesNotExist:
+                # Si no existe, crea un nuevo registro
+                attendance_record = AttendanceRecord.objects.create(
+                    student=student,
+                    shift=current_shift,
+                    status='Falta'  # Inicializa con 'Absent'
+                )
+                created = True
 
             # Verifica si el escaneo ocurre dentro del intervalo de salida
             if current_shift.leave_start and current_shift.leave_end and current_shift.leave_start <= now_time <= current_shift.leave_end:
@@ -66,14 +79,23 @@ class ScanDniView(APIView):
             action = "entry" if created else "exit"
             time = attendance_record.entry_time if action == "entry" else attendance_record.exit_time
             attendance_status = attendance_record.status
-            sendSms(student_dni, action, time, attendance_status)
+            # Antes de llamar a sendSms, verifica si es la primera vez que se registra la entrada
+
+
+            if action == "entry" and created:
+                # Esto significa que es la primera vez que el estudiante registra su entrada
+                # y puedes proceder a enviar el SMS
+                sendSms(student_dni, action, time, attendance_status)
+            elif action == "exit":
+                # Para la salida, siempre envía el SMS, ya que se supone que solo se registra una vez
+                sendSms(student_dni, action, time, attendance_status)
             return SuccessResponse(data_=AttendanceRecordDniSerializer(attendance_record).data).send()
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 def sendSms(student_dni, action, time, status):
     account_sid = 'ACbc58ad6b0a5ac0c5e6964881275e4cbe'
-    auth_token = '4759a8f614c076955f56eed09fad331a'
+    auth_token = '7d871a7ccd93a41ed5a5bd15994977e5'
     client = Client(account_sid, auth_token)
 
     try:
@@ -86,12 +108,17 @@ def sendSms(student_dni, action, time, status):
     local_entry_time = localtime(time, get_default_timezone())
     formatted_time = local_entry_time.strftime('%H:%M')
 
-    body_message = f"\nINFORME DE ASISTENCIA\nNombre: {student.name},\nHora {('Entrada' if action == 'entry' else 'Salida')}: {formatted_time},\nEstado: {status}."
-
-    message = client.messages.create(
-        from_='+16592175883',
-        body=body_message,
-        to=f'+51{parent_phone_number}'
-    )
-
-    print(message.sid)
+    # Construir el mensaje basado en la acción
+    if action == 'entry':
+        body_message = f"\nINFORME DE ASISTENCIA\nNombre: {student.name},\nHora Entrada: {formatted_time},\nEstado: {status}."
+    else:  # Para la acción de salida, no incluir el estado
+        body_message = f"\nINFORME DE ASISTENCIA\nNombre: {student.name},\nHora Salida: {formatted_time}."
+    try:
+        message = client.messages.create(
+            from_='+16592175883',
+            body=body_message,
+            to=f'+51{parent_phone_number}'
+        )
+        print(message.sid)
+    except TwilioRestException as e:
+        print(f"Error al enviar SMS: {e}")
